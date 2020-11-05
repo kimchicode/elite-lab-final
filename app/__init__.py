8d781f34310a11792674cd0f332665cc07dfba6d
김유신 <yushinkim@gim-yusins-mbp.lan>
2020-11-04 20:39:37 -0800

    my message

--- app/__init__.py
+++ app/__init__.py
@@ -15,9 +15,8 @@ if database_url:
 else:
   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
       os.path.join(basedir, '../app.db')
-      
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, '../app.db')
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'fill-in-with-secret-key'
